f11a5a1e01dd926a992dfa5d50edcd2c5c98e926
doubletreeHW <ooongswife@naver.com>
2025-12-13 01:30:10 +0900

    [Silver IV] Title: 덱, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <deque>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	deque<int> D;
+	int n;
+	cin >> n;
+	while(n--) {
+		string s;
+		cin >> s;
+		if(s=="push_front") {
+			int x;
+			cin >> x;
+			D.push_front(x);
+		}
+		else if(s=="push_back") {
+			int x;
+			cin >> x;
+			D.push_back(x);
+		}
+		else if(s=="pop_front") {
+			if(D.empty())
+			 	cout << -1 << '\n';
+			else {
+				cout << D.front() << '\n';
+				D.pop_front();
+			}
+		}
+		else if(s=="pop_back") {
+			if(D.empty()) 
+				cout << -1 << '\n';
+			else {
+				cout << D.back() << '\n';
+				D.pop_back();
+			}
+		}
+		else if(s=="size") {
+			cout << D.size() << '\n';
+		}
+		else if(s=="empty") {
+			cout << D.empty() << '\n';
+		}
+		else if(s=="front") {
+			if(D.empty()) 
+				cout << -1 << '\n';
+			else 
+				cout << D.front() << '\n';
+		}
+		else {
+			if(D.empty()) 
+				cout << -1 << '\n';
+			else 
+				cout << D.back() << '\n';
+		}
+	}
+	return 0;
+}
