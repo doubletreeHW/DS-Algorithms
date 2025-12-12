03b2f32751b036b18711d040e4a1182a44b9008a
doubletreeHW <ooongswife@naver.com>
2025-12-12 19:27:49 +0900

    [Silver IV] Title: 큐 2, Time: 324 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	queue<int> Q;
+
+	int n;
+	cin >> n;
+	while(n--) {
+		string s;
+		cin >> s;
+		if(s=="push") {
+			int x;
+			cin >> x;
+			Q.push(x);
+		}
+		else if(s=="pop") {
+			if(Q.empty()) cout << -1 << '\n';
+			else {
+				cout << Q.front() << '\n';
+				Q.pop();
+			}
+		}
+		else if(s=="size") {
+				cout << Q.size() << '\n';
+		}
+		else if(s=="empty") {
+			if(Q.empty()) cout << 1 << '\n';
+			else  cout << 0 << '\n';
+		}
+		else if(s=="front") {
+			if(Q.empty()) cout << -1 << '\n';
+			else cout << Q.front() << '\n';
+		}
+		else {
+			if(Q.empty()) cout << -1 << '\n';
+			else cout << Q.back() << '\n';
+		}
+	}
+	return 0;
+}
